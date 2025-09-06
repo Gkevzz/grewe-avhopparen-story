665080d9ae746da0ab8c64116691b15dc68a14fc
Gkevzz <kevinvennevold92@gmail.com>
2025-09-07 01:22:57 +0200

    Update Book.tsx

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
-import bookCover from "@/assets/book-cover.jpg";
+import bookCover from "@/assets/avhopparen-en-sann-berattelse.jpg";
 import Navigation from "@/components/Navigation";
 
 const Book = () => {
