3a6ca6d559c12780a1a802ece597d717fd0834e3
Gkevzz <kevinvennevold92@gmail.com>
2025-09-15 18:06:03 +0200

    Update Hero.tsx
    Text förminskning lite mer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
           </div>
           
           {/* Description */}
-          <div className="mt-8 md:mt-16 lg:mt-20">
+          <div className="mt-4 md:mt-16 lg:mt-20">
            <p className="font-body text-base sm:text-lg md:text-xl lg:text-2xl text-white/90 leading-relaxed max-w-lg mx-auto px-4">
   Inspirerande föreläsningar och en gripande självbiografi om vägen från kriminalitet till ett meningsfullt liv.
            </p>
